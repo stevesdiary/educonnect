0cccc416a7e52fc6ca6809f82218246d2df763da
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-22 23:33:57 +0100

    Add config and userRoute

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 const multer = require("multer");
+const db = require("./config/dbConfig");
+const userRoute = require("./routes/userRoute");
 const port = process.env.PORT || 5500;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/user', userRoute);
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
