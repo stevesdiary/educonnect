edb19eb28077c152441b2e705b74b8b239d63f46
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-27 19:32:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ app.use(express.json())
 const multer = require("multer");
 const db = require("./config/dbConfig");
 const userRoute = require("./routes/userRoute");
+const subjectRoute = require("./routes/subjectRoute");
 const questionRoute = require("./routes/questionRoute");
 const answerRoute = require("./routes/answerRoute");
-const loginRoute = require("./routes/loginRoute");
+
 const port = process.env.PORT || 5100;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/login', loginRoute);
 app.use('/user', userRoute);
 app.use('/question', questionRoute);
-// app.use('/answer', answerRoute);
+app.use("/subject", subjectRoute);
+app.use('/answer', answerRoute);
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
