d808fbcba99c2326fe53f68f88b9572bc3f7184a
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-27 19:24:14 +0100

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { validateUser } = require('../middlewares/validate');
 const { userController } = require('../controllers/userController');
+const { authentication } = require("../middlewares/authentication");
 const { validate } = require("../middlewares/validate");
 const { createUserSchema, idSchema } = require('../validator/validator');
+const { loginController } = require('../controllers/loginController');
 
 
-router.post('/create', validate(createUserSchema, 'body'), userController.createUser);
+router.post('/create', userController.createUser);
+router.post("/login", loginController.login);
+router.post("/logout", loginController.logout);
 router.get ('/allusers', userController.getAll);
-router.get('/getone/:id', validate(idSchema, 'param'), userController.getOne);
-router.patch('/update/:id', validate(idSchema, 'param'), userController.updateUser);
-router.delete('/deleteuser/:id', validate(idSchema, 'param'), userController.deleteOne);
+router.get('/getone/:id', userController.getOne);
+router.patch('/update/:id', userController.updateUser);
+router.delete('/delete/:id', userController.deleteOne);
 module.exports = router;
