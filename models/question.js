34bd1fd60bde2d64d8930c8688cffbf4c5e00d87
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-27 19:28:01 +0100

    Change content datatype to text
    This allows for more characters

--- models/question.js
+++ models/question.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
 			allowNull: false
 		},
     content:  {
-			type: DataTypes.STRING,
+			type: DataTypes.TEXT,
 			allowNull: false
 		},
     user_id:  {
