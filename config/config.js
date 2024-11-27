bde354f4f1da32ffde60d7ec6f27b1cea8f9937f
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-27 23:47:56 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,19 +2,26 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.LDB_USER,
-    "password": process.env.LDB_PASSWORD,
-    "database": process.env.LDB_NAME,
-    "host": process.env.LDB_HOST,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
     dialectOptions: {
-      // ssl: {
-      //   require: true,
-      //   rejectUnauthorized: false,
-      //   // ca: process.env.CERTIFICATE
-      // },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+        // ca: process.env.CERTIFICATE
+      },
       "connectTimeout": 30000
     },
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "production": {
     "username": process.env.DB_USER,
