f203a7097653b04f0aee7cefa157da577d29e304
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-28 01:32:16 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -19,10 +19,10 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init({
     id:{
-			type: DataTypes.UUID,
+			type: DataTypes.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
       allowNull: false,
-      defaultValue: DataTypes.UUIDV4,
 		},
     name: {
 			type: DataTypes.STRING,
