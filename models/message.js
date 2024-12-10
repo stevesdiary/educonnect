447e422e84935cee494e9313514e72b5801c69d4
stevesdiary <stephenoyeyemi@gmail.com>
2024-12-11 00:00:14 +0100

    Create message.js

---
+++ models/message.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Message.belongsTo(models.ChatRoom, { foreignKey: 'room_id', as: 'room' });
+      Message.belongsTo(models.User, { foreignKey: "user_id", as: 'sender' });
+    }
+  }
+  Message.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    room_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.ENUM('text', 'image', 'file'),
+      defaultValue: 'text'
+    },
+    status: {
+      type: DataTypes.ENUM('sent', 'delivered', 'read'),
+      defaultValue: 'sent'
+    },
+  }, {
+    sequelize,
+    modelName: 'Message',
+    tableName: 'messages'
+  });
+  return Message;
+};
