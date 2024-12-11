0a35e7aa0f27c8056252a1bd38fe2607cf83f12e
stevesdiary <stephenoyeyemi@gmail.com>
2024-12-11 01:37:20 +0100

    Add get and delete routes

--- routes/subjectRoute.js
+++ routes/subjectRoute.js
@@ -5,6 +5,8 @@ const { validate } = require("../middlewares/validate");
 const { authentication } = require('../middlewares/authentication');
 
 router.post('/create', authentication, subjectController.createSubject);
-
+router.get("/all", authentication, subjectController.getAll);
+router.get("/one/:id", authentication, subjectController.getOne);
+router.delete("/delete/:id", authentication, subjectController.deleteOne);
 
 module.exports = router;
